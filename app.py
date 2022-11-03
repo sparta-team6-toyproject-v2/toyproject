1c75e117eca09836ac76260ed52266b3fc6e4121
yena choi <geniusyena332@gmail.com>
2022-11-03 12:23:34 +0900

    app.py 포트 추가 &  index.html에 초기화 기능 추가 & tg.html a tag herf 수정

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
-from pymongo import MongoClient
 import certifi
 ca = certifi.where()
 client = MongoClient('mongodb+srv://test:sparta@cluster0.rv3ttod.mongodb.net/test', tlsCAFile=ca)
@@ -14,22 +13,6 @@ db = client.hanghae_10_preliminary
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-# @app.route('/list')
-# def list():
-#     return render_template('2.html')
-
-
-# @app.route('/travel', methods=["GET"])
-# def list_get():
-#     cities = ['루체른', '마드리드', '로마', '아테네', '파리',
-#               '하와이', '칸쿤', '뉴욕', '몰디브', '발리', '보라카이', '세부', '다낭']
-#     results = []
-#     for city in cities:
-#         result = db.travel.find_one({'city_kor': city}, {'_id': False})
-#         results.append(result)
-#     return jsonify(results)
 
 
 # 태근님
@@ -50,9 +33,70 @@ def list_get():
 
 
 # 승열님
-@app.route('/sy')
-def home_sy():
-    return render_template('toy_sy.html')
+@app.route('/루체른')
+def home_lucerne():
+    return render_template('toy_sy_루체른.html')
+
+
+@app.route('/마드리드')
+def home_madrid():
+    return render_template('toy_sy_마드리드.html')
+
+
+@app.route('/로마')
+def home_rome():
+    return render_template('toy_sy_로마.html')
+
+
+@app.route('/아테네')
+def home_athens():
+    return render_template('toy_sy_아테네.html')
+
+
+@app.route('/파리')
+def home_paris():
+    return render_template('toy_sy_파리.html')
+
+
+@app.route('/하와이')
+def home_hawaii():
+    return render_template('toy_sy_하와이.html')
+
+
+@app.route('/칸쿤')
+def home_cancun():
+    return render_template('toy_sy_칸쿤.html')
+
+
+@app.route('/뉴욕')
+def home_newyork():
+    return render_template('toy_sy_뉴욕.html')
+
+
+@app.route('/몰디브')
+def home_maldives():
+    return render_template('toy_sy_몰디브.html')
+
+
+@app.route('/발리')
+def home_bali():
+    return render_template('toy_sy_발리.html')
+
+
+@app.route('/보라카이')
+def home_boracay():
+    return render_template('toy_sy_보라카이.html')
+
+
+@app.route('/세부')
+def home_cebu():
+    return render_template('toy_sy_세부.html')
+
+
+@app.route('/다낭')
+def home_danang():
+    return render_template('toy_sy_다낭.html')
+
 
 
 @app.route("/travel", methods=["GET"])
